4b7f2f79ff9127553a32a5e90b4addd4d4bd4cd0
Ken <ken@hypack.com>
2024-12-11 09:34:36 -0500

    Day 11 Part 2

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -3,6 +3,7 @@ import math
 from unittest import skip
 
 #f = open("example.txt", "r")
+
 f = open("input.txt", "r")
 
 
@@ -26,34 +27,46 @@ def part1():
 				else:
 					newstones.append( stone * 2024 )
 		stones = newstones
-		print( f"{i}\n" )
+		#print( f"{i}: {stones}\n" )
 
-	print(f"Total paths: {len(stones)}")
+	print(f"Total stones: {len(stones)}")
 
 
 def part2():
 	print( "Part 2")
-	global stones
-	for i in range(25,75):
+	f.seek(0)
+	#list of stones and stone count
+	stones = [ (int(x),1) for x in f.readline().strip().split() ]
+	for i in range(75):
 		#procss stones
 		newstones = []
 		for stone in stones:
-			if(stone == 0):
-				newstones.append(1)
+			if(stone[0] == 0):
+				newstones.append((1,stone[1]))
 			else:
 				#get number of digits in stone
-				digits = int(math.floor( math.log10(stone)+1 )) 
+				digits = int(math.floor( math.log10(stone[0])+1 )) 
 				#if even
 				if( digits & 1 == 0 ): 
 					fac = math.pow( 10, (digits>>1) )
-					newstones.append( int(stone / fac) )
-					newstones.append( int(stone % fac) )
+					newstones.append( (int(stone[0] / fac),stone[1]) )
+					newstones.append( (int(stone[0] % fac),stone[1]) )
 				else:
-					newstones.append( stone * 2024 )
-		stones = newstones
+					newstones.append( (stone[0] * 2024,stone[1]) )
+		#sort based on stone value
+		newstones.sort( key = lambda x: x[0])
+		stones.clear()
+		for n in newstones:
+			if(len(stones) ==0 or n[0]!=stones[-1][0]):
+				stones.append(n)
+			else:
+				stones[-1] = (stones[-1][0],stones[-1][1]+n[1])
 		print( f"{i}\n" )
+	sum = 0
+	for stone in stones:
+		sum += stone[1]
 
-	print(f"Total paths: {len(stones)}")
+	print(f"Total stones: {sum}")
 	
 part1()
 part2()
